dca0daa53a3f4ce3d66579ff0ddefd431b5d0c5a
Caio Vitor Santos <caiovitor.santos@gmail.com>
2017-03-19 18:52:39 -0300

    Add SignIn event to component

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Angular2TokenService, SignInData } from 'angular2-token';
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _tokenService: Angular2TokenService) { }
+
+  private _signInData: SignInData = <SignInData>{};
+  private _output: any;
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+  	this._output = null;
+  	this._tokenService.signIn(this._signInData).subscribe(
+  		res 	=> {
+  			this._signInData = <SignInData>{};
+  			this._output = res;
+  		},
+  		error => {
+    		this._signInData = <SignInData>{};
+        this._output = error;
+  		});
   }
 
 }
